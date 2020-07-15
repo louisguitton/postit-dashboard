c346bc070f9f90c06618f1da1dd5f9e14b470f6c
Louis Guitton <louisguitton93@gmail.com>
2020-07-15 19:34:32 +0200

    Deploy to Heroku

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express"),
   app = express(),
   http = require("http"),
   socketIo = require("socket.io");
+const PORT = process.env.PORT || 8080
 
 // start webserver on port 8080
 var server = http.createServer(app);
 var io = socketIo.listen(server);
-server.listen(8080);
+server.listen(PORT);
 // add directory with our static files
 app.use(express.static(__dirname + "/public"));
 
